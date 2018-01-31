90a3e5b85e8249e63282f131ca6bb1c2267ae65e
Ivan Kasaija <kasaijaivan@gmail.com>
2018-01-30 22:37:36 -0500

    Second Commit

---
+++ app/assets/javascripts/slider.js
@@ -0,0 +1,13 @@
+jQuery(document).ready(function($) {
+          $('.slider').unslider({
+            dots: true, 
+            keys: true,
+            autoplay: true,
+            arrows: true,
+            infinite: true,
+            animation: 'vertical',
+            autoplay: true,
+            speed:  1000,
+            delay:  3000
+          });
+});
